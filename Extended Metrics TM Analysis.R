2a688aff3110bfd9e65f150e7fc38f8f242da5a3
Chris <jcjabr@gmail.com>
2017-08-24 16:20:00 -0500

    tidying up script

--- Extended Metrics TM Analysis.R
+++ Extended Metrics TM Analysis.R
@@ -13,8 +13,8 @@ ipak <- function(pkg){
   sapply(pkg, require, character.only = TRUE)
 }
 
-ipak("checkpoint")
-checkpoint("2017-07-12")
+# ipak("checkpoint")
+# checkpoint("2017-08-24")
 ipak(c("ggplot2", "tm", "sqldf", "scales","chron", "tidytext", "tidyr"))
 
 
@@ -22,7 +22,7 @@ ipak(c("ggplot2", "tm", "sqldf", "scales","chron", "tidytext", "tidyr"))
 #### IMPORT & CLEAN DATA ####
 
 # em <- read.csv("C:\\Work\\Analysis\\Text Mining\\sample ticket text mining dataset.csv")
-em <- read.csv("\\\\cewp1650\\Chris Jabr Reports\\Extended Metrics 2017.csv")
+em <- read.csv("\\\\cewp1650\\Chris Jabr Reports\\Extended Metrics 2017 sample.csv")
 
 # clean/transform / date calculations #
 colnames(em)[1] <- "Id"
@@ -30,26 +30,28 @@ em$Title <- as.character(em$Title)
 em$Smart_Region <- as.factor(em$Smart_Region)
 em$Created_Date <- as.Date(em$Created_Date, "%m/%d/%Y")
 em$Resolved_Date <- as.Date(em$Resolved_Date, "%m/%d/%Y")
-
+em$Created_Week_Ending <- as.Date(em$Created_Week_Ending, "%m/%d/%Y")
+em$Resolved_Week_Ending <- as.Date(em$Resolved_Week_Ending, "%m/%d/%Y")
 
 # Created/Resolved Week / Month "variables" 
-em$Created_Week_R <- as.Date(cut(em$Created_Date, breaks = "week"))
-em$Created_Month_R <- as.Date(cut(em$Created_Date, breaks = "month"))
-em$Resolved_Week_R <- as.Date(cut(em$Resolved_Date, breaks = "week"))
-em$Resolved_Month_R <- as.Date(cut(em$Resolved_Date, breaks = "month"))
-
-# week and month values of dates
-em$Created.Week <- as.Date(cut(em$Created_Date,
-                               breaks = "week",
-                               start.on.monday = T)) # week starting the Monday. need to somehow get next sunday....
+# em$Created_Week_R <- as.Date(cut(em$Created_Date, breaks = "week"))
+# em$Created_Month_R <- as.Date(cut(em$Created_Date, breaks = "month"))
+# em$Resolved_Week_R <- as.Date(cut(em$Resolved_Date, breaks = "week"))
+# em$Resolved_Month_R <- as.Date(cut(em$Resolved_Date, breaks = "month"))
 
 
 # grep / gsub / regex; further clean title
 em$Title <- gsub("[^a-zA-Z0-9 ]"," ",em$Title)
 
-# slice - just OnePOS Incidents
+# slice - just OnePOS Incidents, then filter out columns for analysis
 em <- sqldf("select * from [em] where ONEPOS_LIST = 1")
-
+exclude.columns <- c("Closed_date_cst","Created_Year","Resolved_Year", "Closed_Week","Closed_Week_Ending",
+                     "L1_to_L2","L2_to_L3","L1_to_L3","L1_to_Hardware",
+                     "CREATED_USER","Title_Region","Title_Location","First_Real_Assign_Date","ONEPOS_LIST"
+                     ,"Time_to_Response__M_","Avg_Time_Per_Analyst__M_","Time_To_First_Assign"
+                     ,"Open_To_Resolve_Time__M_", "multiple_resolve_flag", "NUM_DISTINCT_TEAMS", "NUM_DISTINCT_ASSIGNED",
+                     "First_SG_Grouping", "Last_SG_Grouping")
+em <- em[,-which(names(em) %in% exclude.columns)]
 
 # write.csv(em, file = "em with transformations.csv", row.names = FALSE)
 
@@ -75,7 +77,6 @@ em.tidy.dtm$flag <- ifelse(em.tidy.dtm$count > 0, 1, 0)
 
 # append reset of data to dtm. .Minor cleaning.
 em.tidy.dtm.full <- sqldf("select [em.tidy.dtm].term
-                          ,[em.tidy.dtm].count
                           ,[em.tidy.dtm].flag
                           ,[em].*
                             from [em.tidy.dtm] join [em] on [em.tidy.dtm].document = [em].Id")
@@ -90,8 +91,6 @@ write.csv(em.tidy.dtm.full, file = "em tidy dtm full.csv", row.names = FALSE)
 # file size too big
 # em.dtm.m <- as.matrix(em.dtm)
 # em.plus <- cbind(em, em.dtm.m)
-
-
-write.csv(em.plus, file = "em plus.csv", row.names = FALSE)
+# write.csv(em.plus, file = "em plus.csv", row.names = FALSE)
 
 #### End ####
