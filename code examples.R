ad59b0bc2fa3ef24932b112a8eaaee3b32d862bd
Chris <jcjabr@gmail.com>
2017-11-01 12:52:01 -0500

    ggmap example

--- code examples.R
+++ code examples.R
@@ -1,3 +1,25 @@
+#### 11/1/2017 - plotting coordinates on a map? ####
+
+# resource: https://stackoverflow.com/questions/23130604/plot-coordinates-on-map
+
+# loading the required packages
+library(ggplot2)
+install.packages("ggmap")
+library(ggmap)
+
+# creating a sample data.frame with your lat/lon points
+lon <- c(-38.31,-35.5)
+lat <- c(40.96, 37.5)
+df <- as.data.frame(cbind(lon,lat))
+
+# getting the map
+mapgilbert <- get_map(location = c(lon = mean(df$lon), lat = mean(df$lat)), zoom = 4,
+                      maptype = "satellite", scale = 2)
+
+# plotting the map with some points on it
+ggmap(mapgilbert) +
+  geom_point(data = df, aes(x = lon, y = lat, fill = "red", alpha = 0.8), size = 5, shape = 21) +
+  guides(fill=FALSE, alpha=FALSE, size=FALSE)
 
 
 
