4c5cc100c556f93d16c6cb5c15458826b38af3e1
Chris <jcjabr@gmail.com>
2018-01-19 15:10:08 -0600

    testing regex tokens

--- Scratchpad.R
+++ Scratchpad.R
@@ -27,3 +27,29 @@ test <- v.data %>% unnest_tokens(bigram, POS.Readiness.Status, token = "ngrams",
 
 test %>% count(word1, word2, sort = T)
 test %>% count(bigram.copy, sort = T)
+
+
+# 1/19/2018 test clipboard & text parsing ----
+
+a <- readClipboard()
+a
+str(a)
+grep("\\t", a) # metacharacters left in when data read from clipboard
+a.t <- data_frame(text = a)
+str(a.t)
+a1 <- a.t %>% unnest_tokens(output = word, input = text) %>% count(word, sort = TRUE)
+a2 <- a.t %>% unnest_tokens(output = word, input = text, format = c("text")) %>% count(word, sort = TRUE)
+identical(a1, a2)
+
+# custom words parser (n = 1 word) without filtering out single letter words
+a3 <- a.t %>% unnest_tokens(word, text, token = "regex", pattern = "[ ]+") %>% count(word, sort = TRUE)
+a3
+a3$word[grep("(?i)source", a3$word)]
+
+# r.1: replace entire cell w/ value; r.2: custom ngrams w/ filtering out metadata
+a4 <- a.t %>% unnest_tokens(word, text, token = "regex", pattern = "[ ]+[^[:space:]]+[ ]+[^[:space:]]+") %>%
+  mutate(word.r = replace(word, str_detect(word, "[[:cntrl:]]"), "")) %>%
+  mutate(word.r.2 = str_replace_all(word, "[[:cntrl:]]", ""))
+  count(word, sort = TRUE)
+a4
+View(a4)
