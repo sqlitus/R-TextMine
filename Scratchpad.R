766114c5b45f2a6443fa63c1937481443dccf32b
Chris <jcjabr@gmail.com>
2018-04-19 16:29:16 -0500

    completed phase num parsing from tix title

--- Scratchpad.R
+++ Scratchpad.R
@@ -157,9 +157,9 @@ grep
 
 
 
-#### quick steph NE analysis ----
+#### quick steph NE analysis - PHRASE ANALYSIS ----
 library(tidyverse); library(tidytext); library(readxl)
-read.csv("", encoding = "UTF-8")
+
 scale_data <- readxl::read_excel(path = file.choose(), sheet = "data",trim_ws = TRUE, skip = 3)
 View(scale_data)
 str(scale_data)
@@ -179,39 +179,67 @@ table(scale_data_2$Smart_Region) %>% sum()
 
 
 
-# !!! phrase analysis #
+# !!! phrase analysis: most sophisticated phrase analysis done yet #
 
 # part 1: compile words w/ counts
 scale_data_2 %>% unnest_tokens(ngram, Title, token = "ngrams", n = 2) %>% distinct() %>% count(ngram, sort = TRUE)
 scale_data_2 %>% unnest_tokens(ngram, Title, token = "ngrams", n = 3) %>% distinct() %>% count(ngram, sort = TRUE)
 
+# get top x words by frequency
 scale_data_phrases <- scale_data_2 %>% unnest_tokens(phrase, Title) %>% distinct() %>% count(phrase, sort = TRUE) %>% 
   slice(1:100) %>% mutate(num_words = 1)
 
+
+# append top x ngrams by frequency of 1:10 ngram length
 for (i in 2:10){
   step_df <- scale_data_2 %>% unnest_tokens(phrase, Title, token = "ngrams", n = i) %>% 
     distinct() %>% count(phrase, sort = TRUE) %>% slice(1:100) %>% mutate(num_words = i)
   scale_data_phrases <- scale_data_phrases %>% bind_rows(step_df)
 }
 
+
+# create words dataset to attach calculations to original dimensions
+scale_data_phrases_base <- scale_data_2 %>% unnest_tokens(phrase, Title, drop = FALSE) %>% distinct()
+scale_data_full <- left_join(scale_data_phrases, scale_data_phrases_base, by = "phrase")
+write.csv(scale_data_phrases_base, "scale_data_phrases_base.csv")
+
+
 # part 2: calculate modified frequency
 scale_data_phrases <- scale_data_phrases %>% mutate(adj_freq = n * num_words ^ (1 + num_words / 10))
 scale_data_phrases <- scale_data_phrases %>% 
+  # adjust frequency score (lower) if it is a SINGLE stopword
   mutate(adj_freq_stopwords = case_when(phrase %in% stop_words$word ~ adj_freq * .5, TRUE ~ adj_freq)) %>%
-  mutate(stopword_adjusted = case_when(phrase %in% stop_words$word ~ TRUE))
+  # flag row if it is a SINGLE stopword
+  mutate(stopword_adjusted = case_when(phrase %in% stop_words$word ~ TRUE)) %>%
+  # flag row if it contains ANY stopwords
+  mutate(full_stopword_found = case_when(grepl(paste(paste0("\\b",stop_words$word,"\\b"), collapse = "|"), phrase) ~ "found stopword"))
+  # count how many stopwords found ...
+  # adj freq by how many stopwords found ...
+  # flag if this phrase is fully contained in a higher-scoring phrase
+
 
 scale_data_phrases %>% arrange(desc(adj_freq)) %>% View()
 
 
 
-# NOT RUN {
-x <- 1:50
-case_when(
-  x %% 35 == 0 ~ "fizz buzz",
-  x %% 5 == 0 ~ "fizz",
-  x %% 7 == 0 ~ "buzz",
-  TRUE ~ as.character(x)
-)
+
+# data checking: double counted - tickets w/ words overlap - mentions word 'scale' and 'scales'
+grep("(?i)\\bscale\\b", scale_data$Title) %>% length()
+grep("(?i)\\bscales\\b", scale_data$Title) %>% length()
+
+scale_data %>% filter(grepl("\\bscale\\b", scale_data$Title)) %>% View()
+scale_data %>% filter(grepl("(?i)\\bscale\\b", scale_data$Title) & grepl("(?i)\\bscales\\b", scale_data$Title)) %>% View()
+
+
+
+
+
+# part 3: visualize simple ngrams
+scale_data_phrases
+
+write.csv(scale_data_phrases, "scale_data_phrases.csv")
+
+
 
 
 
@@ -234,3 +262,30 @@ Phrase_df <- function(in_df, the_text_column = "Title", num_ngrams = 10, top_cut
 }
 
 scale_data_phrases <- Phrase_df(scale_data)
+
+
+
+
+
+
+#### 4/18/2018 - Text parsing - PHASE NUM in title ----
+library(tidyverse)
+ONOW_OnePOS_List <- readxl::read_excel(path = "\\\\cewp1650\\Chris Jabr Reports\\ONOW Exports\\incident.xlsx")
+import_time <- date()
+
+# FCF / FDR
+ONOW_OnePOS_List$Cdate <- lubridate::date(ONOW_OnePOS_List$Created)
+ONOW_OnePOS_List$Rdate <- lubridate::date(ONOW_OnePOS_List$Resolved)
+ONOW_OnePOS_List$FCF <- case_when(ONOW_OnePOS_List$Cdate == ONOW_OnePOS_List$Rdate & ONOW_OnePOS_List$`Reassignment count` == 0 ~ 1,
+                                   TRUE ~ 0)
+ONOW_OnePOS_List$FDR <- case_when(ONOW_OnePOS_List$Cdate == ONOW_OnePOS_List$Rdate ~ 1, TRUE ~ 0)
+
+sum(ONOW_OnePOS_List$FCF, na.rm = TRUE)
+sum(ONOW_OnePOS_List$FDR, na.rm = TRUE)
+
+# grab phase # from title
+ONOW_OnePOS_List$Phase_Num = stringr::str_extract(ONOW_OnePOS_List$`Short description`, "\\d½?[.]\\d[.]\\d")
+
+
+write.csv(ONOW_OnePOS_List, na = "", row.names = FALSE, "\\\\cewp1650\\Chris Jabr Reports\\ONOW Exports\\ONOW_OnePOS_Metrics.csv")
+
